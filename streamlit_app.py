a17bda63050161d553f1e77074084d173f5893da
MujiRahayuWidodo <87989295+MujiRahayuWidodo@users.noreply.github.com>
2024-07-01 13:37:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st 
 import pandas as pd
     
+def main() : 
+    # Membuat tab bar dengan dua tab
+    tabs = st.tabs(["Data", "Profile"])
 
+    with tabs[0]:
+        data()
+    with tabs[1]:
+        profile()
+        
 if __name__ == '__main__' : 
   main()
